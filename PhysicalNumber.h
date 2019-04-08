bf3128491efc4b12aefe4ede78855dde762907d0
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-08 17:10:08 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -22,7 +22,7 @@ namespace ariel
         bool operator>(const PhysicalNumber& rhs);
         bool operator<=(const PhysicalNumber& rhs);
         bool operator>=(const PhysicalNumber& rhs);
-        bool  samefamily  (const PhysicalNumber& rhs);
+        int  samefamily  (const PhysicalNumber& rhs);
         double changesize( PhysicalNumber& lhs,PhysicalNumber& rhs);
         //+=,-=,+,-
         PhysicalNumber &operator+=(const PhysicalNumber& rhs);
