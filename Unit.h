a069a3d43b6fe26f21e20580f321698fa6053c70
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-08 15:36:37 +0300

    Create Unit.h

---
+++ Unit.h
@@ -0,0 +1,34 @@
+
+#pragma once
+
+
+
+namespace ariel
+
+{
+
+    enum Unit
+
+    {
+
+        M,
+
+        KM,
+
+        CM,
+
+        SEC,
+
+        MIN,
+
+        HOUR,
+
+        G,
+
+        KG,
+
+        TON
+
+    };
+
+};
