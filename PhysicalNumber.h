98b61712870b067628676dc8dee129c6c7afe6d1
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-08 15:34:43 +0300

    Create PhysicalNumber.h

---
+++ PhysicalNumber.h
@@ -0,0 +1,41 @@
+
+#pragma once
+#include "Unit.h"
+#include <iostream>
+using namespace std;
+namespace ariel
+{
+    class PhysicalNumber
+    {
+    public:
+        double value;
+        Unit unit;
+        PhysicalNumber(double num, Unit type):value(num),unit(type)
+        {
+           value=num;
+            unit=type;
+        }
+        //boolean
+        bool operator==(const PhysicalNumber& rhs);
+        bool operator!=(const PhysicalNumber& rhs);
+        bool operator<(const PhysicalNumber& rhs);
+        bool operator>(const PhysicalNumber& rhs);
+        bool operator<=(const PhysicalNumber& rhs);
+        bool operator>=(const PhysicalNumber& rhs);
+        //+=,-=,+,-
+        PhysicalNumber &operator+=(const PhysicalNumber& rhs);
+        PhysicalNumber &operator-=(PhysicalNumber& rhs);
+        PhysicalNumber &operator+(const PhysicalNumber& rhs);
+        PhysicalNumber &operator-(const PhysicalNumber& rhs);
+        //onary 
+        PhysicalNumber& operator-();
+        PhysicalNumber& operator+();
+        //++,--
+        PhysicalNumber& operator++();
+        PhysicalNumber& operator--();
+        //stream
+        friend std::istream& operator>>(std::istream& in, const PhysicalNumber& a);
+        friend ostream& operator<<(ostream& out, const PhysicalNumber& a);
+       
+    };
+}
