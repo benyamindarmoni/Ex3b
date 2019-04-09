38ce853059996d5c97eea80aca5f546fda15bc44
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-09 13:33:16 +0300

    Update PhysicalNumber.cpp

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -11,22 +11,22 @@ bool PhysicalNumber::operator==(const PhysicalNumber& rhs)
     double b=rhs.value;
     if(family!=0){
       if(family==1){
-         if(unit==0)a=a\100000;
-        if(unit==1)a=a\1000;  
-         if(rhs.unit==0)b=b\100000;
-        if(rhs.unit==1)b=b\1000;
+         if((int)unit==0)a=a\100000;
+        if((int)unit==1)a=a\1000;  
+         if((int)rhs.unit==0)b=b\100000;
+        if((int)rhs.unit==1)b=b\1000;
       }
        else if(family==2){
-          if(unit==3)a=a\120;
-        if(unit==4)a=a\60; 
-          if(rhs.unit==3)b=b\120;
-        if(rhs.unit==4)b=b\60;
+          if((int)unit==3)a=a\120;
+        if((int)unit==4)a=a\60; 
+          if((int)rhs.unit==3)b=b\120;
+        if((int)rhs.unit==4)b=b\60;
        }
        else {
-          if(unit==6)a=a\1000000;
-        if(unit==7)a=a\1000; 
-          if(rhs.unit==6)b=b\1000000;
-        if(rhs.unit==7)b=b\1000;
+          if((int)unit==6)a=a\1000000;
+        if((int)unit==7)a=a\1000; 
+          if((int)rhs.unit==6)b=b\1000000;
+        if((int)rhs.unit==7)b=b\1000;
        }
 
         if(a==b)return true;
@@ -34,16 +34,12 @@ bool PhysicalNumber::operator==(const PhysicalNumber& rhs)
     return false;
     
 }
-double PhysicalNumber:: changesize( PhysicalNumber& lhs,PhysicalNumber& rhs){
-    
-  
-return 0;   
-}
+
 int PhysicalNumber:: samefamily  (const PhysicalNumber& rhs){
-    if(unit>=0&&unit<=2&&rhs.unit>=0&&rhs.unit<=2)return 1;
+    if((int)unit>=0&&(int)unit<=2&&(int)rhs.unit>=0&&(int)rhs.unit<=2)return 1;
   
-   else if(unit>=3&&unit<=5&&rhs.unit>=3&&rhs.unit<=5)return 2;
-      else if(unit>=6&&unit<=8&&rhs.unit>=6&&rhs.unit<=8)return 3;
+   else if((int)unit>=3&&(int)unit<=5&&(int)rhs.unit>=3&&(int)rhs.unit<=5)return 2;
+      else if((int)unit>=6&&(int)unit<=8&&(int)rhs.unit>=6&&(int)rhs.unit<=8)return 3;
     else
        return 0;
 }
@@ -59,22 +55,22 @@ bool PhysicalNumber::operator<(const PhysicalNumber& rhs)
     double b=rhs.value;
     if(family!=0){
       if(family==1){
-         if(unit==0)a=a\100000;
-        if(unit==1)a=a\1000;  
-         if(rhs.unit==0)b=b\100000;
-        if(rhs.unit==1)b=b\1000;
+         if((int)unit==0)a=a\100000;
+        if((int)unit==1)a=a\1000;  
+         if((int)rhs.unit==0)b=b\100000;
+        if((int)rhs.unit==1)b=b\1000;
       }
        else if(family==2){
-          if(unit==3)a=a\120;
-        if(unit==4)a=a\60; 
-          if(rhs.unit==3)b=b\120;
-        if(rhs.unit==4)b=b\60;
+          if((int)unit==3)a=a\120;
+        if((int)unit==4)a=a\60; 
+          if((int)rhs.unit==3)b=b\120;
+        if((int)rhs.unit==4)b=b\60;
        }
        else {
-          if(unit==6)a=a\1000000;
-        if(unit==7)a=a\1000; 
-          if(rhs.unit==6)b=b\1000000;
-        if(rhs.unit==7)b=b\1000;
+          if((int)unit==6)a=a\1000000;
+        if((int)unit==7)a=a\1000; 
+          if((int)rhs.unit==6)b=b\1000000;
+        if((int)rhs.unit==7)b=b\1000;
        }
 
         if(a<b)return true;
@@ -89,22 +85,22 @@ bool PhysicalNumber::operator>(const PhysicalNumber& rhs)
     double b=rhs.value;
     if(family!=0){
       if(family==1){
-         if(unit==0)a=a\100000;
-        if(unit==1)a=a\1000;  
-         if(rhs.unit==0)b=b\100000;
-        if(rhs.unit==1)b=b\1000;
+         if((int)unit==0)a=a\100000;
+        if((int)unit==1)a=a\1000;  
+         if((int)rhs.unit==0)b=b\100000;
+        if((int)rhs.unit==1)b=b\1000;
       }
        else if(family==2){
-          if(unit==3)a=a\120;
-        if(unit==4)a=a\60; 
-          if(rhs.unit==3)b=b\120;
-        if(rhs.unit==4)b=b\60;
+          if((int)unit==3)a=a\120;
+        if((int)unit==4)a=a\60; 
+          if((int)rhs.unit==3)b=b\120;
+        if((int)rhs.unit==4)b=b\60;
        }
        else {
-          if(unit==6)a=a\1000000;
-        if(unit==7)a=a\1000; 
-          if(rhs.unit==6)b=b\1000000;
-        if(rhs.unit==7)b=b\1000;
+          if((int)unit==6)a=a\1000000;
+        if((int)unit==7)a=a\1000; 
+          if((int)rhs.unit==6)b=b\1000000;
+        if((int)rhs.unit==7)b=b\1000;
        }
 
         if(a>b)return true;
@@ -117,24 +113,24 @@ bool PhysicalNumber::operator<=(const PhysicalNumber& rhs)
       int family=samefamily(rhs);
    double a=value;
     double b=rhs.value;
-    if(family!=0){
+    if((family!=0){
       if(family==1){
-         if(unit==0)a=a\100000;
-        if(unit==1)a=a\1000;  
-         if(rhs.unit==0)b=b\100000;
-        if(rhs.unit==1)b=b\1000;
+         if((int)unit==0)a=a\100000;
+        if((int)unit==1)a=a\1000;  
+         if((int)rhs.unit==0)b=b\100000;
+        if((int)rhs.unit==1)b=b\1000;
       }
        else if(family==2){
-          if(unit==3)a=a\120;
-        if(unit==4)a=a\60; 
-          if(rhs.unit==3)b=b\120;
-        if(rhs.unit==4)b=b\60;
+          if((int)unit==3)a=a\120;
+        if((int)unit==4)a=a\60; 
+          if((int)rhs.unit==3)b=b\120;
+        if((int)rhs.unit==4)b=b\60;
        }
        else {
-          if(unit==6)a=a\1000000;
-        if(unit==7)a=a\1000; 
-          if(rhs.unit==6)b=b\1000000;
-        if(rhs.unit==7)b=b\1000;
+          if((int)unit==6)a=a\1000000;
+        if((int)unit==7)a=a\1000; 
+          if((int)rhs.unit==6)b=b\1000000;
+        if((int)rhs.unit==7)b=b\1000;
        }
 
         if(a<=b)return true;
@@ -149,22 +145,22 @@ bool PhysicalNumber::operator>=(const PhysicalNumber& rhs)
     double b=rhs.value;
     if(family!=0){
       if(family==1){
-         if(unit==0)a=a\100000;
-        if(unit==1)a=a\1000;  
-         if(rhs.unit==0)b=b\100000;
-        if(rhs.unit==1)b=b\1000;
+         if((int)unit==0)a=a\100000;
+        if((int)unit==1)a=a\1000;  
+         if((int)rhs.unit==0)b=b\100000;
+        if((int)rhs.unit==1)b=b\1000;
       }
        else if(family==2){
-          if(unit==3)a=a\120;
-        if(unit==4)a=a\60; 
-          if(rhs.unit==3)b=b\120;
-        if(rhs.unit==4)b=b\60;
+          if((int)unit==3)a=a\120;
+        if((int)unit==4)a=a\60; 
+          if((int)rhs.unit==3)b=b\120;
+        if((int)rhs.unit==4)b=b\60;
        }
        else {
-          if(unit==6)a=a\1000000;
-        if(unit==7)a=a\1000; 
-          if(rhs.unit==6)b=b\1000000;
-        if(rhs.unit==7)b=b\1000;
+          if((int)unit==6)a=a\1000000;
+        if((int)unit==7)a=a\1000; 
+          if((int)rhs.unit==6)b=b\1000000;
+        if((int)rhs.unit==7)b=b\1000;
        }
 
         if(a>=b)return true;
@@ -180,51 +176,51 @@ PhysicalNumber& PhysicalNumber::operator+=(const PhysicalNumber& rhs)
     double b=rhs.value;
     if(family!=0){
       if(family==1){
-         if(unit==0)a=a\100000;
-        if(unit==1)a=a\1000;  
-         if(rhs.unit==0)b=b\100000;
-        if(rhs.unit==1)b=b\1000;
+         if((int)unit==0)a=a\100000;
+        if((int)unit==1)a=a\1000;  
+         if((int)rhs.unit==0)b=b\100000;
+        if((int)rhs.unit==1)b=b\1000;
          a+=b;
          value=a;
-         if(unit==0)
+         if((int)unit==0)
          {
             value=a*100000;
          }
-         if(unit==1)
+         if((int)unit==1)
          {
               value=a*1000;
          }
          return *this;
       }
        else if(family==2){
-          if(unit==3)a=a\120;
-        if(unit==4)a=a\60; 
-          if(rhs.unit==3)b=b\120;
-        if(rhs.unit==4)b=b\60;
+          if((int)unit==3)a=a\120;
+        if((int)unit==4)a=a\60; 
+          if((int)rhs.unit==3)b=b\120;
+        if((int)rhs.unit==4)b=b\60;
            a+=b;
           value=a;
-          if(unit==3)
+          if((int)unit==3)
           {
              value=a*120;
           }
-          if(unit==4)
+          if((int)unit==4)
           {
              value=a*60;
           }
           return *this;
        }
        else {
-          if(unit==6)a=a\1000000;
-        if(unit==7)a=a\1000; 
-          if(rhs.unit==6)b=b\1000000;
-        if(rhs.unit==7)b=b\1000;
+          if((int)unit==6)a=a\1000000;
+        if((int)unit==7)a=a\1000; 
+          if((int)rhs.unit==6)b=b\1000000;
+        if((int)rhs.unit==7)b=b\1000;
            a+=b;
           value=a;
-          if(unit==6)
+          if((int)unit==6)
           {
              value=a*1000000;
           }
-          if(unit==7)
+          if((int)unit==7)
           {
              value=a*1000;
           }
@@ -240,51 +236,51 @@ PhysicalNumber& PhysicalNumber::operator-=(PhysicalNumber& rhs)
     double b=rhs.value;
     if(family!=0){
       if(family==1){
-         if(unit==0)a=a\100000;
-        if(unit==1)a=a\1000;  
-         if(rhs.unit==0)b=b\100000;
-        if(rhs.unit==1)b=b\1000;
+         if((int)unit==0)a=a\100000;
+        if((int)unit==1)a=a\1000;  
+         if((int)rhs.unit==0)b=b\100000;
+        if((int)rhs.unit==1)b=b\1000;
          a-=b;
          value=a;
-         if(unit==0)
+         if((int)unit==0)
          {
             value=a*100000;
          }
-         if(unit==1)
+         if((int)unit==1)
          {
               value=a*1000;
          }
          return *this;
       }
        else if(family==2){
-          if(unit==3)a=a\120;
-        if(unit==4)a=a\60; 
-          if(rhs.unit==3)b=b\120;
-        if(rhs.unit==4)b=b\60;
+          if((int)unit==3)a=a\120;
+        if((int)unit==4)a=a\60; 
+          if((int)rhs.unit==3)b=b\120;
+        if((int)rhs.unit==4)b=b\60;
            a-=b;
           value=a;
-          if(unit==3)
+          if((int)unit==3)
           {
              value=a*120;
           }
-          if(unit==4)
+          if((int)unit==4)
           {
              value=a*60;
           }
           return *this;
        }
        else {
-          if(unit==6)a=a\1000000;
-        if(unit==7)a=a\1000; 
-          if(rhs.unit==6)b=b\1000000;
-        if(rhs.unit==7)b=b\1000;
+          if((int)unit==6)a=a\1000000;
+        if((int)unit==7)a=a\1000; 
+          if((int)rhs.unit==6)b=b\1000000;
+        if((int)rhs.unit==7)b=b\1000;
            a-=b;
           value=a;
-          if(unit==6)
+          if((int)unit==6)
           {
              value=a*1000000;
           }
-          if(unit==7)
+          if((int)unit==7)
           {
              value=a*1000;
           }
