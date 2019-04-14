1ad1ac8004698d08e898b4f344849af30ec509c2
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-14 17:03:01 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -80,7 +80,7 @@ namespace ariel
         void convert(double& ,double& ,int ,const PhysicalNumber& );
 
         //stream
-    PhysicalNumber& help(double ,int);
+   
         friend istream& operator>>(istream& in,  PhysicalNumber& a);
 
         friend ostream& operator<<(ostream& out, const PhysicalNumber& a);
