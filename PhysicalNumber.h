8caadfdfafe89818d6bdfb30d795e468b6eebd1d
DZand <48325754+DZand@users.noreply.github.com>
2019-04-14 10:56:05 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -1,46 +1,89 @@
 
 #pragma once
+
 #include "Unit.h"
+
 #include <iostream>
+
 using namespace std;
+
 namespace ariel
+
 {
+
     class PhysicalNumber
+
     {
+
     public:
+
         double value;
+
         Unit unit;
+
         PhysicalNumber(double num, Unit type):value(num),unit(type)
+
         {
-           value=num;
+
+            value=num;
+
             unit=type;
+
         }
+
         //boolean
+
         bool operator==(const PhysicalNumber& rhs);
+
         bool operator!=(const PhysicalNumber& rhs);
+
         bool operator<(const PhysicalNumber& rhs);
+
         bool operator>(const PhysicalNumber& rhs);
+
         bool operator<=(const PhysicalNumber& rhs);
+
         bool operator>=(const PhysicalNumber& rhs);
-               int  samefamily  (const PhysicalNumber& rhs);
+
+        int  samefamily  (const PhysicalNumber& rhs);
+
         //+=,-=,+,-
+
         PhysicalNumber &operator+=(const PhysicalNumber& rhs);
+
         PhysicalNumber &operator-=(const PhysicalNumber& rhs);
+
         PhysicalNumber& operator+(const PhysicalNumber& rhs);
+
         PhysicalNumber& operator-(const PhysicalNumber& rhs);
+
         //onary 
+
         PhysicalNumber& operator-();
+
         PhysicalNumber& operator+();
+
         //++,--
+
         PhysicalNumber& operator++(int);
-         PhysicalNumber& operator++();
+
+        PhysicalNumber& operator++();
+
         PhysicalNumber& operator--(int);
-          PhysicalNumber& operator--();
-          void convert(double& ,double& ,int ,const PhysicalNumber& );
+
+        PhysicalNumber& operator--();
+
+        void convert(double& ,double& ,int ,const PhysicalNumber& );
+
         //stream
-        friend istream& operator>>(std::istream& in,  PhysicalNumber& a);
-         friend ostream& operator<<(ostream& out, PhysicalNumber& a);
-       
+
+        friend istream& operator>>(istream& in,  PhysicalNumber& a);
+
+        friend ostream& operator<<(ostream& out, const PhysicalNumber& a);
+
+
     };
- 
+
+
+
 } 
